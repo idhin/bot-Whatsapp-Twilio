3ec5096e452ab0834f4e3da6bbc1f30feec03c0a
Khulafaur Rasyidin <kultusanmu@gmail.com>
2021-07-29 23:20:04 +0700

    save to file

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
+import csv
 
 from utils import fetch_reply
 
@@ -17,6 +18,23 @@ def sms_reply():
     # Fetch the message
     msg = request.form.get('Body')
     phone_no = request.form.get('From')
+
+    # f = open("tests.txt", "w")
+    # f.write(phone_no + " pesannya : " + msg)
+    # f.close()
+
+    # header = ['nomor HP', 'pesan']
+    data = [msg, phone_no]
+
+    with open('hasil.csv', 'a', encoding='UTF8', newline='\n') as f:
+        writer = csv.writer(f)
+
+        # write the header
+        # writer.writerow(header)
+
+        # write the data
+        writer.writerow(data)
+
     reply = fetch_reply(msg, phone_no)
 
     # Create reply
